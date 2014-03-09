c6f6fc55fef6f080c4ea9786c96c56fe900a14bc
root <root@pris-server5.(none)>
2014-03-09 11:22:39 +0800

    preoder midorder postorder in trees

--- BianLi/Tree.h
+++ BianLi/Tree.h
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
 struct Node
@@ -6,9 +7,13 @@ struct Node
     char name;
     Node * left;
     Node* right;
+    bool firstflag;
     Node(char input)
     {
         name = input;
+        left = NULL;
+        right ;
+        firstflag = false; 
     }
     
 };
@@ -22,5 +27,10 @@ public:
     ~Tree();
     bool CreatTree(Node * &);
     void PreOrder(Node * &);
+    void PreOrder_2(Node *);
+    void MidOrder(Node * &);
+    void MidOrder_2(Node *);
+    void PostOrder(Node * &);
+    void PostOrder_2(Node *);
     
 };
--- BianLi/main.cpp
+++ BianLi/main.cpp
@@ -6,6 +6,16 @@ int main()
     Tree newtree;
     newtree.CreatTree(newtree.root);
     newtree.PreOrder(newtree.root);
+    cout<<endl;
+    newtree.PreOrder_2(newtree.root);
+    cout<<endl;
+    newtree.MidOrder(newtree.root);
+    cout<<endl;
+    newtree.MidOrder_2(newtree.root);
+    cout<<endl;
+    newtree.PostOrder(newtree.root);
+    cout<<endl;
+    newtree.PostOrder_2(newtree.root);
     return 0;
     
 }
